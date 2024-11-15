4b35f594b4e401325ff45c51b4e32d5bb2ee2edd
Akash-kafle <aakashkafle47@gmail.com>
2024-11-15 16:04:10 +0545

    fix: correct import path for Dashboard component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
-import Dashboard from "./components/dashboard";
+import Dashboard from "./components/Dashboard";
 import Login from "./components/Login"
 import Dropoff from "./components/Dropoff";
 
